32f48667c639714423ac8fb077babdbcd41441e7
Niri2003 <141294719+Niri2003@users.noreply.github.com>
2025-09-27 23:34:50 +0530

    Add files via upload

---
+++ lib/global/global_instances.dart
@@ -0,0 +1,8 @@
+import 'package:users_app/viewModel/home_view_model.dart';
+
+import '../viewModel/auth_view_model.dart';
+import '../viewModel/common_view_model.dart';
+
+CommonViewModel commonViewModel = CommonViewModel();
+AuthViewModel authViewModel = AuthViewModel();
+HomeViewModel homeViewModel = HomeViewModel();
